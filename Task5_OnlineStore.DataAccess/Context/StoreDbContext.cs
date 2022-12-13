b4c46adada55474a07ce9a2447e31bb361623858
dtamon <dominiktamon@gmail.com>
2022-12-13 15:39:31 +0100

    entity models and generate schema

---
+++ Task5_OnlineStore.DataAccess/Context/StoreDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task5_OnlineStore.DataAccess.Entities;
+
+namespace Task5_OnlineStore.DataAccess.Context
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Task5_OnlineStore.DataAccess/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_OnlineStore.DataAccess.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Cost { get; set; }
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+    }
+}
